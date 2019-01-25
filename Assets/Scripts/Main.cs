9322b59d04187debdacf5e53ae298428fdea1a18
Jipay <jipay.detiege@gmail.com>
2019-01-25 21:00:33 +0100

    Add Tools package

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Main : MonoBehaviour {
+
+    public float speed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, 0, speed * Time.deltaTime));
+	}
+}

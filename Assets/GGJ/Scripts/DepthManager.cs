b4bb0a3795c4b4371abe3d3e49c4e0e4ba35f057
Jipay <jipay.detiege@gmail.com>
2019-01-25 22:16:01 +0100

    add depth manager + drag system

--- Assets/GGJ/Scripts/DepthManager.cs
+++ Assets/GGJ/Scripts/DepthManager.cs
@@ -1,17 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Tools;
 
-public class DepthManager : MonoBehaviour
+public class DepthManager : MonoSingleton<DepthManager>
 {
-    // Use this for initialization
-    void Start()
-    {
+    [SerializeField] float _startDepth;
+    [SerializeField] float _stepDepth;
 
+    List<DepthObject> _objects = new List<DepthObject>();
+
+    public void Register(DepthObject depthObject)
+    {
+        _objects.Add(depthObject);
+    }
+
+    public void Unregister(DepthObject depthObject)
+    {
+        _objects.Remove(depthObject);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Actualize()
     {
+        _objects.Sort(Compare);
 
+        for (int i = 0; i < _objects.Count; i++)
+        {
+            Vector3 pos = _objects[i].transform.position;
+            pos.z = _startDepth + _stepDepth * (_objects.Count - i);
+            _objects[i].transform.position = pos;
+        }
+    }
+
+    int Compare(DepthObject d0, DepthObject d1)
+    {
+        float d0x = d0.transform.position.x;
+        float d0y = d0.transform.position.y;
+
+        float d1x = d1.transform.position.x;
+        float d1y = d1.transform.position.y;
+
+        if(d0y != d1y)
+        {
+            return d0y.CompareTo(d1y);
+        }
+
+        return d0x.CompareTo(d1x);
     }
 }

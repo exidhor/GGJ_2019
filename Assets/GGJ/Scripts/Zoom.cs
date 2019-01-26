cfffe32ce413ec0a2f8c1c5525f95fd5bf11daa9
Jipay <jipay.detiege@gmail.com>
2019-01-26 15:35:37 +0100

    Add zoom

---
+++ Assets/GGJ/Scripts/Zoom.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    public float minFov = 15f;
+    public float maxFov = 90f;
+    public float sensitivity = 10f;
+ 
+    void Update()
+    {
+        float fov = Camera.main.orthographicSize;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.orthographicSize = fov;
+    }
+}

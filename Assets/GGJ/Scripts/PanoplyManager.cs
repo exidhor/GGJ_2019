8e81ec9bc96b1fae0d88e4cc27e5ad95d19c9901
Jipay <jipay.detiege@gmail.com>
2019-01-27 01:33:41 +0100

    Add name for panoplies

---
+++ Assets/GGJ/Scripts/PanoplyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Tools;
+using System;
+
+[Serializable]
+public class PanoplyData
+{
+    public PanoplyType type;
+    public int score;
+    public int cahos;
+    public string endMessage;
+}
+
+public class PanoplyManager : MonoSingleton<PanoplyManager>
+{
+    [SerializeField] PanoplyData[] _panoplies = new PanoplyData[0];
+
+    public PanoplyData Get(PanoplyType type)
+    {
+        for (int i = 0; i < _panoplies.Length; i++)
+        {
+            if(_panoplies[i].type == type)
+            {
+                return _panoplies[i];
+            }
+        }
+
+        return null;
+    }
+}
